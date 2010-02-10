305d23c72acf6a42f717cc63b8dd79594e45d03b
Jeff McElhannon <jeffpm@gmail.com>
2010-02-10 12:54:02 -0500

    changes

--- account.php
+++ account.php
@@ -4,6 +4,15 @@
 </head>
 <body>
 <?php
+//Collect the variables from the form
+$username = $_POST["username"];
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+
+//If the submit button wasn't pressed, show the form
+if (!isset($_POST['submit'])) {
 ?>
 <form method="post" action="<?php echo $PHP_SELF;?>">
 <label for="username">Username:</label>
@@ -16,5 +25,61 @@
 <input type="password" id="password" name="password" /><br />
 <label for="email">Email:</label>
 <input type="text" id="email" name="email" /><br />
+<input type="submit" value="submit" name="submit">
+</form>
+<?php
+}
+else {
+
+	if (empty($username)|| empty($firstname)|| empty($lastname)|| empty ($password)|| empty($email)){
+		?>
+		
+	
+	<form method="post" action="<?php echo $PHP_SELF;?>">
+
+	<?php
+	echo "<label for=\"username\">Username:</label>";
+	if(empty($username)){
+	echo "<input type=\"text\" id=\"username\" name=\"username\" />Enter a username<br />";
+	}
+	else{
+	echo "<input type=\"text\" id=\"username\" name=\"username\" value=\"$username\" /><br />";
+	}
+
+	echo "<label for=\"firstname\">First name:</label>";
+	if (empty($firstname)){
+		echo "<input type=\"text\" id=\"firstname\" name=\"firstname\" />Enter a first name<br />";
+	}
+	else{
+	echo "<input type=\"text\" id=\"firstname\" name=\"firstname\" value=\"$firstname\" /><br />";
+	}
+
+	echo "<label for=\"lastname\">Last name:</label>";
+	if (empty($lastname)){
+		echo "<input type=\"text\" id=\"lastname\" name=\"lastname\" />Enter a last name<br />";
+	}
+	else{
+	echo "<input type=\"text\" id=\"lastname\" name=\"lastname\" value=\"$lastname\" /><br />";
+	}
+
+	echo "<label for=\"password\">Password:</label>";
+	echo "<input type=\"text\" id=\"password\" name=\"password\" />Enter a password<br />";
+
+	echo "<label for=\"email\">Last name:</label>";
+	if (empty($email)){
+		echo "<input type=\"text\" id=\"email\" name=\"email\" />Enter an email<br />";
+	}
+	else{
+	echo "<input type=\"text\" id=\"email\" name=\"email\" value=\"$email\" /><br />";
+	}
+	echo "<input type=\"submit\" value=\"submit\" name=\"submit\">";
+	echo "</form>";
+	}
+else
+	{
+	echo "Thank you for submitting your form.";
+	}
+}
+?>
 </body>
 </html>
