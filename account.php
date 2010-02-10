23ae2c048d4b6425a3704ddb63aec85b769792b5
Jeff McElhannon <jeffpm@gmail.com>
2010-02-10 11:17:17 -0500

    changes

--- account.php
+++ account.php
@@ -3,7 +3,18 @@
 <title>Account Creation</title>
 </head>
 <body>
-test
-hi
+<?php
+?>
+<form method="post" action="<?php echo $PHP_SELF;?>">
+<label for="username">Username:</label>
+<input type="text" id="username" name="username" /><br />
+<label for="firstname">First name:</label>
+<input type="text" id="firstname" name="firstname" /><br />
+<label for="lastname">Last name:</label>
+<input type="text" id="lastname" name="lastname" /><br />
+<label for="password">Password:</label>
+<input type="password" id="password" name="password" /><br />
+<label for="email">Email:</label>
+<input type="text" id="email" name="email" /><br />
 </body>
 </html>
