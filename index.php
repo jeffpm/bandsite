4b33b4198b8cfb1051926e6a2776ee9c754eee65
jzeitz <jessica.zeitz@gmail.com>
2010-02-11 13:02:57 -0500

    Added main page

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>The Ultimate Band Surf</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<script type="text/javascript" src="calendarDateInput.js" />
+
+<body>
+<div id="wrap">
+    <?php include("header.html"); ?>
+	<div id="main">	
+	
+	
+	
+	</div>
+	
+	<div id="footer"><p>Footer here</p></div>
+</div>
+</body>
+</html>
