4f9d1ff7f305a731937fea4007c6c600966f143b
Jeff McElhannon <jeffpm@gmail.com>
2010-02-11 12:46:53 -0500

    modified login pages

--- login.php
+++ login.php
@@ -1,5 +1,30 @@
 <?php
 include "dbconnect.php";
+//if the submit button was not pressed, show the form
+if (!isset($_POST['submit'])) {
+	?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+ <HEAD>
+  <TITLE>Log In </TITLE>
+  <META NAME="Generator" CONTENT="EditPlus">
+  <META NAME="Author" CONTENT="">
+  <META NAME="Keywords" CONTENT="">
+  <META NAME="Description" CONTENT="">
+ </HEAD>
+ 
+<form method="post" action="<?php echo $PHP_SELF;?>">
+<!--<form method="post" action="login.php">-->
+<label for="username">Enter Username:</label>
+<input type="text" id="username" name="username" /><br />
+<label for="password">Enter Password:</label>
+<input type="password" id="password" name="password" /><br />
+<input type="submit" value="submit" name="submit">
+</form>
+<?php
+}
+else
+{
 session_start();
   $username=$_POST["username"];
   $password=$_POST["password"];
@@ -9,12 +34,32 @@ $result=mysqli_query($db, $query);
 
 $num=mysqli_num_rows($result);
 if ($num<1){
-	header("location:login.html");
-
+	//header("location:login.php");
+	?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+ <HEAD>
+  <TITLE>Log In </TITLE>
+  <META NAME="Generator" CONTENT="EditPlus">
+  <META NAME="Author" CONTENT="">
+  <META NAME="Keywords" CONTENT="">
+  <META NAME="Description" CONTENT="">
+ </HEAD>
+<form method="post" action="<?php echo $PHP_SELF;?>">
+<!--<form method="post" action="login.php">-->
+Your username/password combination was incorrect. Please try again.
+<br>
+<label for="username">Enter Username:</label>
+<input type="text" id="username" name="username" /><br />
+<label for="password">Enter Password:</label>
+<input type="password" id="password" name="password" /><br />
+<input type="submit" value="submit" name="submit">
+</form>
+<?php
 }
 	else {
 		session_register("myusername");
 		header("location:account.php");
-
 		}
+}
 	?>
