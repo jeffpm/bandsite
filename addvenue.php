f6bbf6ab4b3c3b40c7c48c7fbdda023d21b0993e
crandles <crandles@mail.umw.edu>
2010-02-18 21:02:36 -0500

    venue adding now using stylesheet

--- addvenue.php
+++ addvenue.php
@@ -1,7 +1,15 @@
-<html>
-<head> </head>
-<body>
-<form method="post" action="createVenue.php">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+ <HEAD>
+  <TITLE>Add a Venue</TITLE>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+ </HEAD>
+ 
+ <body>
+ <div id="wrap">
+     <?php include("header.html"); ?>
+	<div id="main">	
+<form method="post" action="reportvenue.php">
 	<label for="name">Venue Name:</label>
 		<input type="text" id="name" name="name" /> <br />
 	<label for="address">Address:</label>
@@ -10,11 +18,15 @@
 		<input type="text" id="city" name="city" /> <br />
 	<label for="state">State:</label>
 		<input type="text" id="state" name="state" /> <br />
-	<label for="zip">Zipcode:</label>
+	<label for="zip">Zip Code:</label>
 		<input type="text" id="zip" name="zip" /> <br />
     Short description of your venue:<br>
     	<textarea id="description" name="description" rows="8" cols="54" ></textarea><br />
 	<input type="submit" value="Add Venue" name="submit" />
 </form>
+
+</div>	
+	<div id="footer"><p>Footer here</p></div>
+</div>
 </body>
 </html>
--- reportvenue.php
+++ reportvenue.php
@@ -1,5 +1,14 @@
-<html>
-<body> 
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+ <HEAD>
+  <TITLE>Add a Venue</TITLE>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+ </HEAD>
+ 
+ <body>
+ <div id="wrap">
+     <?php include("header.html"); ?>
+	<div id="main">	
 <?php
 	include "dbconnect.php";
 	$name = $_POST['name'];
@@ -12,32 +21,37 @@
 	/*echo " (test code) PASSED VARS: $name $address $city $state $zip $description";*/
 	if(is_null($name) or $name == ""){
 		$valid_responses = false;
-		$name = "Invalid Entry";
+		$namestatus = "*Invalid Entry";
+		$name="";
 	}
 	if(is_null($address) or $address == ""){
 		$valid_responses = false;
-		$address = "Invalid Entry";
+		$addressstatus = "*Invalid Entry";
+		$address="";
 	}
 	if(is_null($city) or $city == ""){
 		$valid_responses = false;
-		$city = "Invalid Entry";
+		$citystatus = "*Invalid Entry";
+		$city="";
 	}
 	if(is_null($state) or $state == ""){
 		$valid_responses = false;
-		$state = "Invalid Entry";
+		$statestatus = "*Invalid Entry";
+		$state="";
 	}
 	if(is_null($zip) or $zip == "" or !is_numeric($zip)){
 		$valid_responses = false;
-		$zip = "Invalid Entry";
+		$zipstatus = "*Invalid Entry";
+		$zip="";
 	}
 	if($valid_responses == false){
 		/*echo "<br>Invalid Submission<br>";*/
-		echo "<form method=\"post\" action=\"createVenue.php\"><label for=\"name\">Venue Name:</label>
-		<input type=\"text\" id=\"name\" name=\"name\" value=\"$name\" /> <br /><label for=\"address\">Address:</label>
-		<input type=\"text\" id=\"address\" name=\"address\" value=\"$address\" /> <br /><label for=\"city\">City:</label>
-		<input type=\"text\" id=\"city\" name=\"city\" value=\"$city\" /> <br /><label for=\"state\">State:</label>
-		<input type=\"text\" id=\"state\" name=\"state\" value=\"$state\" /> <br />
-		<label for=\"zip\">Zipcode:</label><input type=\"text\" id=\"zip\" name=\"zip\" value=\"$zip\" /> 
+		echo "<form method=\"post\" action=\"reportvenue.php\"><label for=\"name\">Venue Name:</label>
+		<input type=\"text\" id=\"name\" name=\"name\" value=\"$name\" /> $namestatus<br /><label for=\"address\">Address:</label>
+		<input type=\"text\" id=\"address\" name=\"address\" value=\"$address\" /> $addressstatus<br /><label for=\"city\">City:</label>
+		<input type=\"text\" id=\"city\" name=\"city\" value=\"$city\" /> $citystatus<br /><label for=\"state\">State:</label>
+		<input type=\"text\" id=\"state\" name=\"state\" value=\"$state\" /> $statestatus<br />
+		<label for=\"zip\">Zip Code:</label><input type=\"text\" id=\"zip\" name=\"zip\" value=\"$zip\" /> $zipstatus
 		<br /> Short description of your venue:<br>
     	<textarea id=\"description\" name=\"description\" rows=\"8\" cols=\"54\" >$description</textarea><br />
 		<input type=\"submit\" value=\"Add Venue\" name=\"submit\" /></form>";
@@ -48,5 +62,8 @@
 			echo "Thank you for submitting your venue location!";
 	}
 ?>
+</div>	
+	<div id="footer"><p>Footer here</p></div>
+</div>
 </body>
 </html>
