d40bfd8f6ce7afede6a5bdffd01c9d093741ed08
jzeitz <jessica.zeitz@gmail.com>
2010-02-21 20:38:51 -0500

    index, login, addVenue homepages complete

--- addvenue.php
+++ addvenue.php
@@ -1,5 +1,7 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<HTML>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
  <HEAD>
   <TITLE>Add a Venue</TITLE>
 	<link rel="stylesheet" type="text/css" href="style.css" />
@@ -26,7 +28,7 @@
 </form>
 
 </div>	
-	<<?php include("footer.html"); ?>
+	<?php include("footer.html"); ?>
 </div>
 </body>
 </html>
