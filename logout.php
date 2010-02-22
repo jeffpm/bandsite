57d03542cc56b8223cd851fc5d7b52834f22df7a
Jeff McElhannon <jeffpm@gmail.com>
2010-02-21 20:57:02 -0500

    some changes

--- logout.php
+++ logout.php
@@ -1,29 +1,5 @@
 <?php
-/*
 session_start();
 session_destroy();
-header("location:index.php");
-*/
+header("location:logout.html");
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<HTML>
- <HEAD>
-  <TITLE>Add a Venue</TITLE>
-	<link rel="stylesheet" type="text/css" href="style.css" />
-	<SCRIPT LANGUAGE="JavaScript">
-  function redireccionar() {
-    setTimeout("location.href='index.php'", 3000);
-  }
-  </SCRIPT>
- </HEAD>
- 
- <body onLoad="redireccionar()">
- <div id="wrap">
-     <?php include("header.html"); ?>
-	<div id="main">	
-	Logged out. Redirecting to index.php in 3 seconds...
-	</div>	
-	<div id="footer"><p>Footer here</p></div>
-</div>
-</body>
-</html>
--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 if(!session_is_registered(myusername)){
-header("location:account.php");
+header("location:index.php");
 }
 ?>
