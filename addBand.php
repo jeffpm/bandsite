d2ccbf0c713110385c23465c3f5ca6a5664f6bcc
Jeff McElhannon <jeffpm@gmail.com>
2010-02-23 11:05:42 -0500

    updated header and add band validation

--- addBand.php
+++ addBand.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if(!session_is_registered(myusername)){
+header("location:login.php");
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
