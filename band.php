60c1466ad575c743eb7fc5fffdc7e2583780bdb1
Jeff McElhannon <jeffpm@gmail.com>
2010-02-23 15:34:59 -0500

    Displays random band and can now view a band

--- band.php
+++ band.php
@@ -1,5 +1,16 @@
 <?php
 include "dbconnect.php";
+$id = $_GET['id'];
+
+$query = "select * from bands where id='$id'";
+	$result = mysqli_query($db, $query)
+	  or die("Error querying Database");
+	
+	$row = mysqli_fetch_array($result);
+	$id = $row['id'];
+	$bandname = $row['bandname'];
+	$members = $row['members'];
+	$description = $row['description'];
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -7,7 +18,9 @@ include "dbconnect.php";
 
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>The Ultimate Band Surf</title>
+  <?php
+  echo "<title>Band Profile For $id</title>";
+  ?>
   <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 
@@ -17,24 +30,12 @@ include "dbconnect.php";
 
     <?php include("sidebar.php"); ?>	
 	<div>
-	Featured Band	
-	</div>
 	<?php
-	$query = "select * from bands ORDER BY RAND() LIMIT 1";
-	$result = mysqli_query($db, $query)
-	  or die("Error querying Database");
-	
-	$row = mysqli_fetch_array($result);
-	$id = $row['id'];
-	$bandname = $row['bandname'];
-	$members = $row['members'];
-	$description = $row['description'];
-	echo "Band: <a href=\"band.php?id=$id\">$bandname</a> <br>";
+	echo "Band: $bandname <br>";
 	echo "Members: $members <br>";
 	echo "Description: $description <br>";
-
 	?>
-	
+	</div>
 	<?php include("footer.html"); ?>
 </div>
 </body>
