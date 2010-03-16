01842567fbf6cfe3b0ca68e2aa61972f92e9878f
crandles <crandles@mail.umw.edu>
2010-03-16 12:46:45 -0400

    pictures work

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
 				<table cellpadding="5">
 					<tr>
 					<?php //to be used later <img src="images/(php) echo "$picture"; (/php)"  /> ?>
-					<td><a href="band.php?id=<?php echo "$bandid"; ?>"><img src="images/tempBand.jpg"></a></td>
+					<td><a href="band.php?id=<?php echo "$bandid"; ?>"><img src="images/<?php echo"$picture"; ?>"></a></td>
 					<td>
 						<?php
 							echo "<p><a href=\"band.php?id=$bandid\">$bandname</a> <br></p>";
@@ -75,7 +75,7 @@
 				<table cellpadding="5">
 					<tr>
 					<?php //to be used later <img src="images/(php) echo "$picture"; (/php)"  /> ?>
-					<td><a href="venue.php?id=<?php echo "$venueid"; ?>"><img src="images/tempVenue.jpg"></a></td>
+					<td><a href="venue.php?id=<?php echo "$venueid"; ?>"><img src="images/<?php echo"$picture"; ?>"></a></td>
 					<td>
 						<?php
 							echo "<p><a href=\"venue.php?id=$venueid\">$name</a> <br></p>";
