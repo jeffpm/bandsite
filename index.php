653a14201bcfe13fd2c0b413934818dcc290cfc3
jzeitz <jessica.zeitz@gmail.com>
2010-03-23 12:49:18 -0400

    changed picture size on main page

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ include "dbconnect.php";
 				<table cellpadding="5">
 					<tr>
 					<?php //to be used later <img src="images/(php) echo "$picture"; (/php)"  /> ?>
-					<td><a href="band.php?id=<?php echo "$bandid"; ?>"><img src="images/<?php echo"$picture"; ?>"></a></td>
+					<td><a href="band.php?id=<?php echo "$bandid"; ?>"><img src="images/<?php echo"$picture"; ?>" width=375 height=350></a></td>
 					<td>
 						<?php
 							echo "<p><a href=\"band.php?id=$bandid\">$bandname</a> <br></p>";
@@ -101,7 +101,7 @@ include "dbconnect.php";
 				<table cellpadding="5">
 					<tr>
 					<?php //to be used later <img src="images/(php) echo "$picture"; (/php)"  /> ?>
-					<td><a href="venue.php?id=<?php echo "$venueid"; ?>"><img src="images/<?php echo"$picture"; ?>"></a></td>
+					<td><a href="venue.php?id=<?php echo "$venueid"; ?>"><img src="images/<?php echo"$picture"; ?>" width=375 height=350></a></td>
 					<td>
 						<?php
 							echo "<p><a href=\"venue.php?id=$venueid\">$name</a> <br></p>";
